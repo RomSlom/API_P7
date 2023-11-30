ef300b5ed903e74934b73e297983b18bcaec28f8
Romain Slomczynski <slomczynskiromain@yahoo.fr>
2023-12-01 02:39:12 +0300

    rtr

--- other_pages/Prediction.py
+++ other_pages/Prediction.py
@@ -5,14 +5,14 @@ from urllib.request import urlopen
 import json
 
 # load our best model
-PATH = "C:\Users\DELL\Formation OC\API_P7\Datas"
+PATH = "C:\Users\DELL\Formation OC\API_P7\Datas\"
 
 #Load Dataframe
 
 
 # X_test=pd.read_csv(PATH+'X_test.csv')
 # y_test=pd.read_csv(PATH+'y_test.csv')
-dataframe=pd.read_csv(PATH+'\dFreduced.csv')
+dataframe=pd.read_csv(''./Datas/dFreduced.csv')
 
 # Main sections
 header = st.container()
